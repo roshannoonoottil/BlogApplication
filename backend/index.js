3fb5b019b07b85c16f788ef577d2bc942747ee51
Roshan Eldho Benny <roshannoonoottil@gmail.com>
2025-05-28 18:51:16 +0530

    Revert "signup controller created"
    This reverts commit a1b500443770615b34ea4d30c014950f13728822.

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,14 @@
-import express from 'express';
-import userRouter from './router/userRoute.js'
-import { connectDB } from "./server/models/userModel.js";
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
 
-connectDB();
-
 // Test route
 app.get('/', (req, res) => {
   res.send('Hello from Node.js backend!');
 });
-
-app.use('/user', userRouter);
 
 // Start server
 app.listen(PORT, () => {
--- backend/router/userRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import userController from '../server/controllers/userController.js';
-
-
-router.post('/signup', userController.signup)
-
-export default router;
