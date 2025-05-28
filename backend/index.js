6687eb752a1876520ff66c569c34bd8e3e69a67e
Roshan Eldho Benny <roshannoonoottil@gmail.com>
2025-05-28 18:52:07 +0530

    Reapply "signup controller created"
    This reverts commit 3fb5b019b07b85c16f788ef577d2bc942747ee51.

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,20 @@
-const express = require('express');
+import express from 'express';
+import userRouter from './router/userRoute.js'
+import { connectDB } from "./server/models/userModel.js";
 const app = express();
 const PORT = 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
 
+connectDB();
+
 // Test route
 app.get('/', (req, res) => {
   res.send('Hello from Node.js backend!');
 });
+
+app.use('/user', userRouter);
 
 // Start server
 app.listen(PORT, () => {
